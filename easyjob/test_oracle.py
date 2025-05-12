8156491bf483030e95e09d1f2acec542161568e0
Roxellana <di.huanchicay@duocuc.cl>
2025-05-11 23:00:13 -0400

    oracle

--- easyjob/test_oracle.py
+++
@@ -1,9 +0,0 @@
-
-import oracledb
-
-# Cambia esta ruta a donde tengas instalado Oracle Instant Client
-oracledb.init_oracle_client(lib_dir="Z:\Documentos\Trabajos\instantclient-basic-windows.x64-23.8.0.25.04\instantclient_23_8")
-
-conn = oracledb.connect(user="ADMIN", dsn="sumativa_high")
-print("✅ Conexión exitosa")
-conn.close()
