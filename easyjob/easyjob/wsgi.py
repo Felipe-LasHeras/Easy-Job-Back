6776d4e565f451cd855003de57dece6d97b7e266
Roxellana <di.huanchicay@duocuc.cl>
2025-05-11 23:05:22 -0400

    oracle

--- easyjob/easyjob/wsgi.py
+++ easyjob/easyjob/wsgi.py
@@ -1,19 +1,12 @@
 import os
 import oracledb
+
+
 oracledb.init_oracle_client(lib_dir=r"Z:\Documentos\Trabajos\instantclient-basic-windows.x64-23.8.0.25.04\instantclient_23_8")
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'easyjob.settings')
-
-from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'easyjob.settings')
 
 
-
-
 from django.core.wsgi import get_wsgi_application
-
-
-
-
 application = get_wsgi_application()
